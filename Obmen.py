7a9064a1f09fc328ed2ee7bcb459bf6c50d9bc6f
ynest <nesterovaulia053@gmail.com>
2024-10-07 16:56:22 +0300

    Создана функция exchenge для получения из интернета курста валют

--- Obmen.py
+++ Obmen.py
@@ -4,7 +4,23 @@ from tkinter import *
 from tkinter import messagebox as mb
 
 
-def exchange
+def exchange():
+    code = entry.get()
+
+    if code:
+        try:
+            response = requests.get('https://open.er-api.com/v6/latest/USD')
+            response.raise_for_status()
+            data = response.json()
+            if code in data['rates']:
+                exchange_rate = data['rates'][code]
+                mb.showinfo("Курс обмена", f"Курс: {exchange_rate} {code} зф 1$")
+            else:
+                mb.showerror("Ошибка!", f"Валюта {code} не найдена")
+        except Exception as e:
+            mb.showerror("Ошибка!", f"Произошла ошибка: {e}.")
+    else:
+        mb.showwarning("Внимание!", "Введите код валюты")
 
 
 window = Tk()
