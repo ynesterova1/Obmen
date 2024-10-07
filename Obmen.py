bcaa2a7ea58ab2c30fe506fef3918db62f23187d
ynest <nesterovaulia053@gmail.com>
2024-10-07 19:12:09 +0300

    Добавлена метка с названием базовой валюты

--- Obmen.py
+++ Obmen.py
@@ -5,10 +5,16 @@ from tkinter import messagebox as mb
 from tkinter import ttk
 
 
-def update_c_label(event):
+def update_b_label(event):
+    code = b_combobox.get()
+    name = cur[code]
+    b_label.config(text=name)
+
+
+def update_t_label(event):
     code = t_combobox.get()
     name = cur[code]
-    c_label.config(text=name)
+    t_label.config(text=name)
 
 
 def exchange():
@@ -51,18 +57,20 @@ window.title("Курс обмена валют")
 window.geometry("360x300")
 
 Label(text="Базовая валюта").pack(padx=10, pady=10)
-
 b_combobox = ttk.Combobox(values=list(cur.keys()))
 b_combobox.pack(padx=10, pady=10)
+b_combobox.bind("<<ComboboxSelected>>", update_b_label)
+b_label = ttk.Label()
+b_label.pack(padx=10, pady=10)
+
 
 Label(text="Целевая валюта").pack(padx=10, pady=10)
-
 t_combobox = ttk.Combobox(values=list(cur.keys()))
 t_combobox.pack(padx=10, pady=10)
-t_combobox.bind("<<ComboboxSelected>>", update_c_label)
+t_combobox.bind("<<ComboboxSelected>>", update_t_label)
 
-c_label = ttk.Label()
-c_label.pack(padx=10, pady=10)
+t_label = ttk.Label()
+t_label.pack(padx=10, pady=10)
 
 Button(text="Получить курс обмена", command=exchange).pack(padx=10, pady=10)
 
